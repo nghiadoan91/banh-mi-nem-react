c151151c5dc72cd54ae61472a72de4b3b8bf5021
Nghia Doan <nghiadoan@hotmail.com>
2022-10-30 19:18:47 +0700

    bb

--- function.js
+++ function.js
@@ -1,6 +1,5 @@
 const { https } = require("firebase-functions");
 const { default: next } = require("next");
-import Router from 'next/router'
 
 const isDev = process.env.NODE_ENV !== "production";
 
@@ -9,22 +8,8 @@ const server = next({
   compression: false,
   conf: { distDir: ".next" }
 });
-import { NextResponse, NextRequest } from 'next/server'
-export async function middleware(req, ev) {
-    const { pathname } = req.nextUrl
-    if (pathname == '/') {
-        return NextResponse.redirect('/en')
-    }
-    return NextResponse.next()
-}
+
 const nextjsHandle = server.getRequestHandler();
 exports.nextServer = https.onRequest((req, res) => {
-  return server.prepare().then(() => {
-    const { pathname } = req.nextUrl
-    if (pathname == '/') {
-        return NextResponse.redirect('/en')
-    }else{
-      return nextjsHandle(req, res);
-    }
-  })
+  return server.prepare().then(() => nextjsHandle(req, res));
 });
