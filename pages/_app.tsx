211cc6d22a8d3240df1d45f46d3ba6495f4ea7d1
Nghia Doan <nghiadoan@hotmail.com>
2022-10-30 22:10:17 +0700

    add facebook pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -56,8 +56,22 @@ export default function App({ Component, pageProps }: AppProps): JSX.Element {
               gtag('config', 'G-R77RD6RWK5', {
               page_path: window.location.pathname,
               });
+
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '603849159757376');
+              fbq('track', 'PageView');
           `}
       </Script>
+      <noscript><img height="1" width="1" style={{display:"none"}}
+src="https://www.facebook.com/tr?id=603849159757376&ev=PageView&noscript=1"
+/></noscript>
       <Head>
         <title>Banh Mi Nem</title>
         <meta
