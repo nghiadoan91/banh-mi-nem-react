a8932331a58d501ef31c9bf45e43757fbd486646
Nghia Doan <nghiadoan@hotmail.com>
2022-11-12 13:47:01 +0700

    fix menu

--- src/views/GuideView/GuideView.tsx
+++ src/views/GuideView/GuideView.tsx
@@ -1,18 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Button, Grid } from "@material-ui/core";
 import { LearnMoreLink } from "../../components/atoms";
 import { SectionHeader } from "../../components/molecules";
-import { Section } from "../../components/organisms";
+import { Section, SectionAlternate } from "../../components/organisms";
 import { Image } from "components/atoms";
+import { uShopBKKfireStore } from "Config";
+import { CatalogShop } from "views/Catalog/components";
 
 const useStyles = makeStyles(theme => {
   const toolbar = theme.mixins.toolbar as any;
   return {
     section: {
-      paddingTop: 0,
-      paddingBottom: 0,
-      alignItems: "center"
+      paddingTop: 10,
+      paddingBottom: 10
+    },
+    sectionAlternate: {
+      paddingTop: 10,
+      paddingBottom: 10
     },
     image: {
       objectFit: "cover",
@@ -23,11 +28,20 @@ const useStyles = makeStyles(theme => {
   };
 });
 
-const GuideView = (): JSX.Element => {
+const GuideView = (): 
+JSX.Element => {
+  const [menus, setMenus] = useState<any>([]);
   useEffect(() => {
-    setTimeout(() => {
-      if (location.hash) location = location;
-    }, 200);
+    console.log("fetch....");
+    uShopBKKfireStore
+      .collection("MenuPage")
+      .doc("Config")
+      .get()
+      .then(result => {
+        if (result.data() != null) {
+          setMenus(result.data().menus);
+        }
+      });
   }, []);
   const classes = useStyles();
 
@@ -37,18 +51,38 @@ const GuideView = (): JSX.Element => {
 
   return (
     <div>
-      <Section className={classes.section}>
-        <>
-          <Image className={classes.image}
-            src="https://i.pinimg.com/originals/e4/83/7f/e4837f4ec07e6bf89e9dcad050c47f30.jpg"
-          />
-          <Image className={classes.image}
-            src="https://i.pinimg.com/originals/a8/50/39/a8503976f868adf604fa934464e1888e.jpg"
-          />
-       </>
-      </Section>
+      {menus.map((item: any, index: number) => {
+        if (index % 2 === 0) {
+          return (
+            <Section key={index} className={classes.section}>
+              <div>
+                <SectionHeader title={item.title} align="center" />
+                <Image className={classes.image}
+                   src={item.image}
+                />
+              </div>
+            </Section>
+          );
+        } else {
+          return (
+            <SectionAlternate
+              disablePadding={true}
+              key={index}
+              className={classes.sectionAlternate}
+            >
+              <div>
+                <SectionHeader title={item.title} align="center" />
+                <Image className={classes.image}
+                  src={item.image}
+                />
+              </div>
+            </SectionAlternate>
+          );
+        }
+      })}
     </div>
   );
 };
 
 export default GuideView;
+
